d399150b5fa5a988afc06d113d32900495583b57
kaareltiitt6nts <kaarel-tiit.tonts@voco.ee>
2024-12-12 08:42:47 +0000

    wip

--- ylesanded/ex_pood/scripts/main.js
+++ ylesanded/ex_pood/scripts/main.js
@@ -1,21 +1,12 @@
-// classes
-import { Product, products } from "./constructors/product.js"
-import { Cart, cart } from "./constructors/cart.js"
-import { Order } from "./constructors/order.js"
-import { Customer, customer } from "./constructors/customer.js"
-
-// views
+import { products } from "./constructors/product.js"
 import { displayProducts } from "./views/products.js"
-
-// router
-import { createNavButtons } from "./nav.js"
 import { navigateTo } from "./router.js"
-import { getViewData } from "./localstorage.js"
+import { createNavButtons } from "./nav.js"
 
 document.title = "E-Pood"
 
 const init = async () => {
-    createNavButtons()
+    createNavButtons() // imelik
     displayProducts(products)
 
     document.querySelector("#shopName").onclick = () => navigateTo("products", products)
